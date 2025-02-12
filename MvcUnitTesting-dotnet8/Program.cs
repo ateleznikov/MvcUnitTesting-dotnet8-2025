5c5050783ddead860ac27c56cf91db78eb648535
Anton Teleznikov <S00240505@atu.ie>
2025-02-12 15:33:59 +0300

    S00240505 Week 2 Lab 1 Project setup

--- MvcUnitTesting-dotnet8/Program.cs
+++ MvcUnitTesting-dotnet8/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using MvcUnitTesting_dotnet8.Models;
+using Tracker.WebAPIClient;
 
 namespace MvcUnitTesting_dotnet8
 {
@@ -7,6 +8,8 @@ namespace MvcUnitTesting_dotnet8
     {
         public static void Main(string[] args)
         {
+            ActivityAPIClient.Track(StudentID: "S00240505", StudentName: "Anton Teleznikov", activityName: "Rad302 2025 Week 2 Lab 1", Task: "Running Week 2 App");
+            
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
